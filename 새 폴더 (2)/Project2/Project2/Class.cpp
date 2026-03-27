7d01601e6f8324ff6301b23afa6bf12b3d186375
jiseong0123 <kjs07965@gmail.com>
2026-03-27 10:43:11 +0900

    20260327

---
+++ 새 폴더 (2)/Project2/Project2/Class.cpp
@@ -0,0 +1,13 @@
+#include "Complex.h"
+
+#include "Complex.h"
+void main()
+{
+	Complex a, b, c;
+	a.read( "A =");
+	b.read( "B =");
+	c.add(a, b);
+	a.print( "A = ");
+	b.print("B = ");
+	c.print("A+B = ");
+}
